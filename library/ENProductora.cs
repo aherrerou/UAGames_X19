5f5b8a6108f5cb04ec1469bc8a8d55a234e4e1a7
Clara Gonzalez Sanchez <cgsg4@gcloud.ua.es>
2023-04-16 15:43:26 +0200

    Creado webForm de prueba mas EN necesarias

---
+++ library/ENProductora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENProductora
+    {
+
+        private int _Id;
+        private string _Nombre;
+        private string _Descripcion;
+        private string _Imagen;
+        private string _Web;
+
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+
+        public string Imagen
+        {
+            get { return _Imagen; }
+            set { _Imagen = value; }
+        }
+
+        public string Web
+        {
+            get { return _Web; }
+            set { _Web = value; }
+        }
+    }
+}
