e869b1d86d442a5411812f67c104b79e77c239aa
Iker SÁNCHEZ ZARAGOZA <iszs1@alu.ua.es>
2023-04-18 21:50:10 +0200

    Por terminar CADCesta

--- Libreria/Libreria/CADCesta.cs
+++ Libreria/Libreria/CADCesta.cs
@@ -19,13 +19,37 @@ namespace Libreria
             conect = new SqlConnection(conexionBBDD);
         }
 
-        public bool createCesta(ENCesta ces) {
+        public bool createCesta(ENCesta ces, ENVidejuego videojuego, ENUsuario usuario) {
 
-            bool res = true;
-
-        
-
+            try
+            {
+                this.conect.Open();
+                string date = ces.Fecha.ToString("yyyy-MM-dd HH:mm:ss");
+                string query = "Insert into Cesta (usuarioID,VideojuegoID,fecha) values" + "(" + usuario.id + "," + videojuego.id + "(convert(datetime, '" + date + "', 120);";
+                SqlCommand com = new SqlCommand(query, conect);
+                com.ExecuteNonQuery();
             }
+            catch (SqlException sqlex)
+            {
+                
+                Console.WriteLine("User operation has failed. Error: {0}", sqlex.Message);
+                return false;
+                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("User operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                this.conect.Close();
+            }
+
+            return true;
+        }
 
 
 
