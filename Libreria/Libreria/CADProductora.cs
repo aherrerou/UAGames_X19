9d51292f5bfbcea57eafd2deac11c77d4097f031
Raúl Luján Domenech <rld8@mscloud.ua.es>
2023-04-18 17:52:36 +0200

    Actualizada la clase ENProductora

---
+++ Libreria/Libreria/CADProductora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+
+namespace Libreria
+{
+    class CADProductora
+    {
+       
+        public CADProductora()
+        {
+            constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+        }
+
+            }
+}
---
+++ Libreria/Libreria/ENProductora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    class ENProductora
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public string Web { get; set; }
+        public ENProductora()
+        {
+            Id = -1;
+            Nombre = "";
+            Descripcion = "";
+            Imagen = "";
+            Web = "";
+        }
+        public ENProductora(int id,string nombre , string descripcion,string imagen ,string web)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Imagen = imagen;
+            Web = web;
+        }
+        public bool createProductora()
+        {
+            bool creada = false;
+            CADProductora nuevaProductora = new CADProductora();
+
+            return creada;
+        }
+    }
+}
