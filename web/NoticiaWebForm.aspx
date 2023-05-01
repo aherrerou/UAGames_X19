ab521ecb667449b8b5a7fc26c22269bdaeef8926
Raúl Luján Domenech <rld8@mscloud.ua.es>
2023-05-01 19:05:47 +0200

    noticia medio hecha

--- web/NoticiaWebForm.aspx
+++ web/NoticiaWebForm.aspx
@@ -1,5 +1,34 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="NoticiaWebForm.aspx.cs" Inherits="web.Noticia" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="NoticiaWebForm.aspx.cs" Inherits="web.ProductoraWebForm" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="main" runat="server">
+    <div>
+       <div>
+        <p>
+            Id: &nbsp;<asp:TextBox ID="text_id" runat="server" Height="20px" style="margin-top: 5px; margin-left: 25px;" Width="200px"></asp:TextBox>
+        </p>
+        <p>
+            Titulo: &nbsp;<asp:TextBox ID="text_titulo" runat="server" Height="20px" style="margin-top: 5px; margin-left: 25px;" Width="200px"></asp:TextBox>
+        </p>
+           <p>
+            Fecha de publicación : &nbsp;<asp:TextBox ID="Text_fecha_public" runat="server" Height="20px" style="margin-top: 5px; margin-left: 25px;" Width="200px"></asp:TextBox>
+        </p>
+        <p>
+            Contenido: &nbsp;<asp:TextBox ID="text_contenido" runat="server" Height="20px" style="margin-top: 5px; margin-left: 25px;" Width="200px"></asp:TextBox>
+        </p>
+           
+           <p>
+            ProductoraId: &nbsp;<asp:TextBox ID="text_productora_id" runat="server" Height="20px" style="margin-top: 5px; margin-left: 25px;" Width="200px"></asp:TextBox>
+        </p>
+           <asp:Label ID="outputMsg" runat="server"></asp:Label><br />
+            <asp:Button text="Crear"  ID="buttom_Crear" runat="server" />
+           <asp:Button text="Mostrar todas las productoras"  ID="bt_Mostrar" runat="server" />
+    </div>
+    </div>
 </asp:Content>
+
+
+
--- web/NoticiaWebForm.aspx.designer.cs
+++ web/NoticiaWebForm.aspx.designer.cs
@@ -11,7 +11,79 @@ namespace web
 {
 
 
-    public partial class Noticia
+    public partial class ProductoraWebForm
     {
+
+        /// <summary>
+        /// Control text_id.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox text_id;
+
+        /// <summary>
+        /// Control text_titulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox text_titulo;
+
+        /// <summary>
+        /// Control Text_fecha_public.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Text_fecha_public;
+
+        /// <summary>
+        /// Control text_contenido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox text_contenido;
+
+        /// <summary>
+        /// Control text_productora_id.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox text_productora_id;
+
+        /// <summary>
+        /// Control outputMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label outputMsg;
+
+        /// <summary>
+        /// Control buttom_Crear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttom_Crear;
+
+        /// <summary>
+        /// Control bt_Mostrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bt_Mostrar;
     }
 }
