2f2d86a0819ae506c0714172534b1edd5539e463
Clara Gonzalez Sanchez <cgsg4@gcloud.ua.es>
2023-05-01 10:41:44 +0200

    Arreglados botones y menu

--- web/Site1.Master.designer.cs
+++ web/Site1.Master.designer.cs
@@ -42,6 +42,24 @@ namespace web
         protected global::System.Web.UI.WebControls.Menu menu;
 
         /// <summary>
+        /// Control BtnIniciarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnIniciarSesion;
+
+        /// <summary>
+        /// Control BtnRegistrarse.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnRegistrarse;
+
+        /// <summary>
         /// Control main.
         /// </summary>
         /// <remarks>
