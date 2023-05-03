18597bcc0908efaea23e2830a0899c48ef700510
Raúl Luján Domenech <rld8@mscloud.ua.es>
2023-05-03 19:24:21 +0200

    prueba

--- web/NoticiaWebForm.aspx.designer.cs
+++ web/NoticiaWebForm.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace web
 {
 
 
-    public partial class ProductoraWebForm
+    public partial class NoticiaWebForm
     {
 
         /// <summary>
--- web/ProductoraWebForm.aspx.designer.cs
+++ web/ProductoraWebForm.aspx.designer.cs
@@ -15,84 +15,12 @@ namespace web
     {
 
         /// <summary>
-        /// Control text_id.
+        /// Control Button1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox text_id;
-
-        /// <summary>
-        /// Control text_Nombre.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox text_Nombre;
-
-        /// <summary>
-        /// Control Text_descripcion.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Text_descripcion;
-
-        /// <summary>
-        /// Control text_Edad.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox text_Edad;
-
-        /// <summary>
-        /// Control Text_Imagen.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Text_Imagen;
-
-        /// <summary>
-        /// Control TextBox1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
-        /// Control outputMsg.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label outputMsg;
-
-        /// <summary>
-        /// Control buttom_Crear.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button buttom_Crear;
-
-        /// <summary>
-        /// Control bt_Mostrar.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button bt_Mostrar;
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
