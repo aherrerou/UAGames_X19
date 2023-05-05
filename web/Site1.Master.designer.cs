a9761a5eb96e394e463398d5aea9c4c6fb0541b4
Clara Gonzalez Sanchez <cgsg4@gcloud.ua.es>
2023-05-05 15:31:41 +0200

    Botones a inicio sesion y registrarse cambiados a hyperlink

--- web/Site1.Master.designer.cs
+++ web/Site1.Master.designer.cs
@@ -48,7 +48,7 @@ namespace web
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button BtnIniciarSesion;
+        protected global::System.Web.UI.WebControls.HyperLink BtnIniciarSesion;
 
         /// <summary>
         /// Control BtnRegistrarse.
@@ -57,7 +57,7 @@ namespace web
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button BtnRegistrarse;
+        protected global::System.Web.UI.WebControls.HyperLink BtnRegistrarse;
 
         /// <summary>
         /// Control main.
